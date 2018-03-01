f27b9f961881e82e901d7b2b19ae07d45cd8ddca
philgrayphilgray <thephilgray@gmail.com>
2018-02-28 19:45:03 -0500

    Render the App component.

--- 001_react-for-beginners/src/index.js
+++ 001_react-for-beginners/src/index.js
@@ -1,7 +1,7 @@
-import React, { Component } from "react";
+import React from "react";
 import { render } from "react-dom";
 
 import "./css/style.css";
-import StorePicker from "./components/StorePicker";
+import App from "./components/App";
 
-render(<StorePicker />, document.querySelector("#main"));
+render(<App />, document.querySelector("#main"));
