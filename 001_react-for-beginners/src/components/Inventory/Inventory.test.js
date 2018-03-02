640b2f470fd8cd939dd50145afc4b11ce0f5d618
philgrayphilgray <thephilgray@gmail.com>
2018-03-01 21:36:16 -0500

    Pass down addFish method to AddFishForm.

--- 001_react-for-beginners/src/components/Inventory/Inventory.test.js
+++ 001_react-for-beginners/src/components/Inventory/Inventory.test.js
@@ -2,9 +2,14 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import Inventory from './Inventory';
+import AddFishForm from '../AddFishForm/AddFishForm';
 
 const inventory = shallow(<Inventory />);
 
 it('renders', () => {
   expect(inventory).toMatchSnapshot();
+});
+
+it('renders `AddFishForm` component', () => {
+  expect(inventory.find(AddFishForm).exists()).toBe(true);
 });
