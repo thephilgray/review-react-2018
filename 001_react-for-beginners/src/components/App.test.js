9292cfbd198a1f4a26b8d069163c00a114adecbb
philgrayphilgray <thephilgray@gmail.com>
2018-03-01 20:46:20 -0500

    Test that App renders Header, Inventory, and Order components.

--- 001_react-for-beginners/src/components/App.test.js
+++ 001_react-for-beginners/src/components/App.test.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+
 import App from './App';
+import Header from './Header/Header';
+import Order from './Order/Order';
+import Inventory from './Inventory/Inventory';
 
 const app = shallow(<App />);
 it('renders correctly', () => {
   expect(app).toMatchSnapshot();
+});
+
+it('renders a `Header` component', () => {
+  expect(app.find(Header).length).toBe(1);
+});
+
+it('renders a `Order` component', () => {
+  expect(app.find(Order).length).toBe(1);
+});
+
+it('renders a `Inventory` component', () => {
+  expect(app.find(Inventory).exists()).toBe(true);
 });
