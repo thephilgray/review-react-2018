d7355fcb7e39571df1238fc3975c33578281958c
philgrayphilgray <thephilgray@gmail.com>
2018-03-01 20:48:17 -0500

    Implement input ref, onSubmit handler, and pushstate.

--- 001_react-for-beginners/src/components/StorePicker/StorePicker.js
+++ 001_react-for-beginners/src/components/StorePicker/StorePicker.js
@@ -2,12 +2,21 @@ import React, { Component } from 'react';
 import { getFunName } from '../../helpers';
 
 class StorePicker extends Component {
+  goToStore = e => {
+    e.preventDefault();
+    const storeName = this.textInput.value;
+    this.props.history.push(`/store/${storeName}`);
+  };
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
         <input
           type="text"
+          ref={input => {
+            this.textInput = input;
+          }}
           required
           placeholder="Store"
           defaultValue={getFunName()}
