6b78ebcb9318ef18a53d34d0dc4f78af5a265103
philgrayphilgray <thephilgray@gmail.com>
2018-03-03 18:49:52 -0500

    Add render test.

--- 001_react-for-beginners/src/components/AddFishForm/AddFishForm.test.js
+++ 001_react-for-beginners/src/components/AddFishForm/AddFishForm.test.js
@@ -1,1 +1,10 @@
 // TODO: test the form inputs and that the App component updates its state with the new fish
+
+import React from 'react';
+import { shallow } from 'enzyme';
+import AddFishForm from './AddFishForm';
+
+it('renders', () => {
+  const wrapper = shallow(<AddFishForm />);
+  expect(wrapper).toMatchSnapshot();
+});
