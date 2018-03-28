d023362fd46193884c628d63a7c8a3c56ca26b2d
philgrayphilgray <thephilgray@gmail.com>
2018-03-28 11:15:37 -0400

    Hookup store to app

--- 003_redux/src/index.js
+++ 003_redux/src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from './App';
+import store from './store/store';
 import registerServiceWorker from './registerServiceWorker';
-
 const app = (
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
-
 ReactDOM.render(app, document.getElementById('root'));
 registerServiceWorker();
